1eba01bc2dc2c2785047f65236deefec1a139d31
naamashvalb <naamah.shv@gmail.com>
2024-07-07 00:35:06 +0300

    fix filename

--- Server/routes/usersRoute.js
+++ Server/routes/usersRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { UsersController } = require('../controllers/userscontroller.js');
+const { UsersController } = require('../controllers/usersController.js');
 const { UsersService } = require("../services/usersService.js");
 const { validateUserData, validate } = require("../middleware/validationMiddleware.js");
 const { validationResult } = require("express-validator");
