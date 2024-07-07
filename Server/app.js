3ca4022fa4274aea85171de38af6ea5b908096f7
naamashvalb <naamah.shv@gmail.com>
2024-07-07 09:27:35 +0300

    clean server files 2

--- Server/app.js
+++ Server/app.js
@@ -5,8 +5,8 @@ const registerRouter = require('./routes/registerRoute.js');
 const loginRouter = require('./routes/loginRoute.js');
 const strategyRouter = require('./routes/strategyRoute.js');
 const submitFormRouter = require('./routes/submitFormRoute.js');
-const exampleRouter = require('./routes/exampleRoute.js'); // Added line
-const gptresponseRouter = require('./routes/gptresponseRoute.js'); // Added line
+const exampleRouter = require('./routes/exampleRoute.js');
+const gptresponseRouter = require('./routes/gptresponseRoute.js');
 
 const authenticateToken = require('./middleware/authenticateToken.js');
 const cors = require('cors');
@@ -16,14 +16,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/example', exampleRouter);
-app.use('/gptresponse', gptresponseRouter);
-app.use('/strategy', strategyRouter);
-app.use('/submitForm', submitFormRouter);
+
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 
 app.use(authenticateToken);
 app.use('/users', usersRouter);
+app.use('/gptresponse', gptresponseRouter);
+app.use('/strategy', strategyRouter);
+app.use('/submitForm', submitFormRouter);
+
 
 app.listen(process.env.PORT, () => console.log(`Listening on port: ${process.env.PORT}`));
